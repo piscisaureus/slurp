66a4baa7942bbd974d0459642a849992b47ea03f
Bert Belder <bertbelder@gmail.com>
2011-08-16 04:45:07 +0200

    Correct file mode

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var fs = require('fs'),
     path = require('path');
 
 // Make sure the log directory exists
-LOG_DIR = path.resolve(path.dirname(process.argv[1]), 'log');
+LOG_DIR = path.resolve(fs.realpathSync(path.dirname(process.argv[1])), 'log');
 
 if (!path.existsSync(LOG_DIR)) {
-  fs.mkdirSync(LOG_DIR, 0666);
+  fs.mkdirSync(LOG_DIR, 0777);
 }
 
 require('./logger.js');
